031fd78d44059dcba4ee3f3be6911ae38ea4f06b
Apoorva P <62649785+ApoorvaP16@users.noreply.github.com>
2023-11-17 12:56:18 +0530

    Updated

--- e-school/e-school/src/main/java/com/cts/eschool/ESchoolApplication.java
+++ e-school/e-school/src/main/java/com/cts/eschool/ESchoolApplication.java
@@ -12,14 +12,14 @@ public class ESchoolApplication {
 
 		ConfigurableApplicationContext ss = SpringApplication.run(ESchoolApplication.class, args);
 		StudentService s = ss.getBean(StudentService.class);
-		s.addNewStudentDetail(1, "Tarun", 78);
-		s.addNewStudentDetail(2, "Ganesh", 45);
-		s.addNewStudentDetail(3, "Abhi", 98);
+		s.addNewStudentDetail(101, "Apoorva", 80);
+		s.addNewStudentDetail(102, "Alex", 92);
+		s.addNewStudentDetail(103, "Park", 65);
 
 		s.getAllStudentDetails();
-		s.getStudentDetailsById(2);
-		s.updateStudentScore(2,50.8);
-		s.deleteStudentDetails(1);
+		s.getStudentDetailsById(101);
+		s.updateStudentScore(101,85);
+		s.deleteStudentDetails(102);
 		s.getAllStudentDetails();
 	}
 
